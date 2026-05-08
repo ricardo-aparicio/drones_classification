8ab1d5df31684bf9369a8dbc53f2ae7f6b097f1f
ricardo-aparicio <ricardo.aparicio1642@gmail.com>
2026-05-08 05:02:55 -0600

    Update to 6 classes build_spectrograms.py

--- build_spectrograms.py
+++ build_spectrograms.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import shutil
 
 BASE = Path(".")  # DETECTIONV1/dataset
-OUT = BASE / "spectrograms_rc_24_58_fly21" 
+OUT = BASE / "spectrograms_rc_24_58_fly24_c_unopes" 
 
 AUTEL_FOLDERS = [ 
     "autel_envuelo_auto_01", #2999 images dron flying in anechoic chamber with RC
@@ -160,42 +160,134 @@ MAVIC4_PRO_FOLDERS = [
     "mavic4pro_envuelo_40m_5G_center_test",   #400
 ]
 
+MAVIC3_PRO_FOLDERS = [
+#     # --- 2.4G ---
+    "mavic3pro_envuelo_10m_center_train", #1999 2.442 GHz
+    "mavic3pro_envuelo_10m_center_val",   #500 2.442 GHz
+    "mavic3pro_envuelo_10m_center_test",  #500 2.442 GHz 
+    "mavic3pro_envuelo_10m_right_train",  #1999 2.442 GHz 
+    "mavic3pro_envuelo_10m_right_val",    #500 2.442 GHz 
+    "mavic3pro_envuelo_10m_right_test",   #499 2.442 GHz 
+    "mavic3pro_envuelo_10m_left_train",   #1999 2.442 GHz 
+    "mavic3pro_envuelo_10m_left_val",     #500 2.442 GHz 
+    "mavic3pro_envuelo_10m_left_test",    #499 2.442 GHz 
+
+    "mavic3pro_envuelo_20m_center_train", #1999 2.442 GHz 
+    "mavic3pro_envuelo_20m_center_val",   #500 2.442 GHz 
+    "mavic3pro_envuelo_20m_center_test",  #499 2.442 GHz 
+    "mavic3pro_envuelo_20m_right_train",  #1999 2.442 GHz 
+    "mavic3pro_envuelo_20m_right_val",    #500 2.442 GHz 
+    "mavic3pro_envuelo_20m_right_test",   #499 2.442 GHz 
+    "mavic3pro_envuelo_20m_left_train",   #1999 2.442 GHz 
+    "mavic3pro_envuelo_20m_left_val",     #500 2.442 GHz 
+    "mavic3pro_envuelo_20m_left_test",    #499 2.442 GHz 
+
+    # --- 5G ---
+    "mavic3pro_envuelo_10m_5G_left_train",    #1999 5.787 GHz 
+    "mavic3pro_envuelo_10m_5G_left_val",      #500 5.787 GHz 
+    "mavic3pro_envuelo_10m_5G_left_test",     #499 5.787 GHz 
+    "mavic3pro_envuelo_10m_5G_right_train",   #1999 5.787 GHz 
+    "mavic3pro_envuelo_10m_5G_right_val",     #500 5.787 GHz 
+    "mavic3pro_envuelo_10m_5G_right_test",    #499 5.787 GHz 
+    "mavic3pro_envuelo_10m_5G_center_train",  #1999 5.787 GHz 
+    "mavic3pro_envuelo_10m_5G_center_val",    #500 5.787 GHz 
+    "mavic3pro_envuelo_10m_5G_center_test",   #499 5.787 GHz 
+
+    "mavic3pro_envuelo_20m_5G_left_train",    #1999 5.787 GHz 
+    "mavic3pro_envuelo_20m_5G_left_val",      #500 5.787 GHz 
+    "mavic3pro_envuelo_20m_5G_left_test",     #499 5.787 GHz 
+    "mavic3pro_envuelo_20m_5G_right_train",   #1999 5.787 GHz 
+    "mavic3pro_envuelo_20m_5G_right_val",     #500 5.787 GHz 
+    "mavic3pro_envuelo_20m_5G_right_test",    #499 5.787 GHz 
+    "mavic3pro_envuelo_20m_5G_center_train",  #1999 5.787 GHz 
+    "mavic3pro_envuelo_20m_5G_center_val",    #500 5.787 GHz 
+    "mavic3pro_envuelo_20m_5G_center_test",   #499 5.787 GHz 
+]
+
 FPV_FOLDERS = [
     "dron_unopes_2", #1499 images in anechoic chamber without RC
     "dron_unopes_3", #1499 images in anechoic chamber with RC
     "dron_unopes_5", #1499 images in anechoic chamber with RC
     "dron_unopes_13", #1499 images in anechoic chamber with RC
     "dron_unopes_15", #1499 images in real ambient environment with RC, spinning propellers
+    "fpv_train",  #2499 images (nuevas)
+    "fpv_val",    #1499 images (nuevas)
+    "fpv_test",   #1499 images (nuevas)
 
 ]
 
 BACKGROUND_FOLDERS = [
-    "ruido_camara01_30db", #1499 images in anechoic chamber
-    "background_24G_real_01", #1999 images in real ambient environment with 2.4G signal
-    "background_24G_real_02", #1999 images in real ambient environment with 2.4G signal
-    "background_24G_real_03", #1999 images in real ambient environment with 2.4G signal (outside, with more interference)
-    "background_24G_real_04", #3999 TRAIN
-    "background_24G_real_05", #3999 TRAIN
-    "background_24G_real_06", #999 VAL
-    "background_24G_real_07", #999 TEST
-    "background_24G_real_08", #999 VAL
-    "background_24G_real_09", #999 TEST
-    "background_5G_real_01", # 1999 images in real ambient environment with 5G signal
-    "background_5G_real_02", # 1999 images in real ambient environment with 5G signal
-    "background_5G_real_03", # 1999 images in real ambient environment with 5G signal (outside, with more interference)
-    "background_5G_real_04", #3999 TRAIN
-    "background_5G_real_05", #999 VAL
-    "background_5G_real_06", #999 TEST
-    "background_5G_real_07", #3999 TRAIN
-    "background_5G_real_08", #999 VAL
-    "background_5G_real_09", #999 TEST
-    "background_negatives_01", #1499 images in real ambient environment without drones in 5.787 GHz with some interference
+    "ruido_camara01_30db", #1499 - 600 = 899 images in anechoic chamber 2.442 GHZ TRAIN 
+    "background_24G_real_01", #1999 - 1100 = 899 images in real ambient environment with 2.442 GHz signal TRAIN 
+    "background_24G_real_02", #1999 - 1200 = 799 images in real ambient environment with 2.442 GHz signal VAL 
+    "background_24G_real_03", #1999 - 1200 = 799 images in real ambient environment with 2.442 GHz signal (outside, with more interference) TEST 
+    "background_24G_real_04", #3999 - 2100 = 1899 TRAIN 2.442 GHz 
+    "background_24G_real_05", #3999 - 2100 = 1899 TRAIN 2.442 GHz 
+    "background_24G_real_06", #999 - 400 = 599 VAL 2.442 GHz 
+    "background_24G_real_07", #999 - 400 = 599 TEST 2.442 GHz 
+    "background_24G_real_08", #999 - 400 = 599 VAL 2.442 GHz 
+    "background_24G_real_09", #999 - 400 = 599 TEST 2.442 GHz 
+    "background_24G_real_10", #700 TRAIN 2.427 GHz ADENTRO
+    "background_24G_real_10b", #1400 TRAIN 2.427 GHz AFUERA 
+    "background_24G_real_10c", #400 TRAIN 2.427 GHz ADENTRO 
+    "background_24G_real_11", #150 TEST 2.427 GHz AFUERA
+    "background_24G_real_11a", #700 TEST 2.427 GHz AFUERA 
+    "background_24G_real_12", #150 VAL 2.427 GHz ADENTRO
+    "background_24G_real_12b", #400 VAL 2.427 GHz ADENTRO 
+    "background_24G_real_12c", #300 VAL 2.427 GHz AFUERA 
+    "background_24G_real_13", #700 TRAIN 2.455 GHz ADENTRO
+    "background_24G_real_13b", #1400 TRAIN 2.455 GHz AFUERA 
+    "background_24G_real_13c", #400 TRAIN 2.455 GHz ADENTRO 
+    "background_24G_real_14", #150 TEST 2.455 GHz AFUERA
+    "background_24G_real_14a", #700 TEST 2.455 GHz AFUERA 
+    "background_24G_real_15", #150 VAL 2.455 GHz ADENTRO
+    "background_24G_real_15b", #400 VAL 2.455 GHz ADENTRO 
+    "background_24G_real_15c", #300 VAL 2.455 GHz AFUERA 
+    "background_5G_real_01", # 1999 - 1100 = 899 images in real ambient environment with 5.787 GHz signal TRAIN 
+    "background_5G_real_02", # 1999 - 1200 = 799 images in real ambient environment with 5.787 GHz signal VAL 
+    "background_5G_real_03", # 1999 -1200 = 799 images in real ambient environment with 5.787 GHz signal (outside, with more interference) TEST 
+    "background_5G_real_04", #3999 - 2100 = 1899 TRAIN 5.787 GHz 
+    "background_5G_real_05", #999 - 400 = 599 VAL 5.787 GHz 
+    "background_5G_real_06", #999 - 400 = 599 TEST 5.787 GHz 
+    "background_5G_real_07", #3999 - 2100 = 1899 TRAIN 5.787 GHz 
+    "background_5G_real_08", #999 - 400 = 599 VAL 5.787 GHz 
+    "background_5G_real_09", #999 - 400 = 599 TEST 5.787 GHz 
+    "background_negatives_01", #1499 - 600 = 899 images in real ambient environment without drones in 5.787 GHz with some interference TRAIN
+    "background_5G_real_10", #700 images in real ambient environment without drones in 5.756 GHz TRAIN ADENTRO
+    "background_5G_real_10b", #1400 images in real ambient environment without drones in 5.756 GHz TRAIN AFUERA 
+    "background_5G_real_10c", #400 images in real ambient environment without drones in 5.756 GHz TRAIN ADENTRO 
+    "background_5G_real_11", #150 images in real ambient environment without drones in 5.756 GHz TEST AFUERA
+    "background_5G_real_11a", #700 images in real ambient environment without drones in 5.756 GHz TEST AFUERA 
+    "background_5G_real_12", #150 images in real ambient environment without drones in 5.756 GHz VAL ADENTRO
+    "background_5G_real_12b", #400 images in real ambient environment without drones in 5.756 GHz VAL ADENTRO 
+    "background_5G_real_12c", #300 images in real ambient environment without drones in 5.756 GHz VAL AFUERA 
+    "background_5G_real_13", #700 images in real ambient environment without drones in 5.818 GHz TRAIN ADENTRO
+    "background_5G_real_13b", #1400 images in real ambient environment without drones in 5.818 GHz TRAIN AFUERA 
+    "background_5G_real_13c", #400 images in real ambient environment without drones in 5.818 GHz TRAIN ADENTRO 
+    "background_5G_real_14", #150 images in real ambient environment without drones in 5.818 GHz VAL ADENTRO
+    "background_5G_real_14b", #400 images in real ambient environment without drones in 5.818 GHz VAL ADENTRO 
+    "background_5G_real_14c", #300 images in real ambient environment without drones in 5.818 GHz VAL AFUERA 
+    "background_5G_real_15", #150 images in real ambient environment without drones in 5.818 GHz TEST AFUERA
+    "background_5G_real_15a", #700 images in real ambient environment without drones in 5.818 GHz TEST AFUERA 
+    "background_5G_real_16", #700 images in real ambient environment without drones in 5.645 GHz TRAIN ADENTRO
+    "background_5G_real_16b", #1400 images in real ambient environment without drones in 5.645 GHz TRAIN AFUERA 
+    "background_5G_real_16c", #400 images in real ambient environment without drones in 5.645 GHz TRAIN ADENTRO 
+    "background_5G_real_17", #150 images in real ambient environment without drones in 5.645 GHz VAL ADENTRO
+    "background_5G_real_17b", #400 images in real ambient environment without drones in 5.645 GHz VAL ADENTRO 
+    "background_5G_real_17c", #300 images in real ambient environment without drones in 5.645 GHz VAL AFUERA 
+    "background_5G_real_18", #150 images in real ambient environment without drones in 5.645 GHz TEST AFUERA
+    "background_5G_real_18a", #700 images in real ambient environment without drones in 5.645 GHz TEST AFUERA 
+    "background_5G_5645_hardneg_train", #700 images in real ambient environment without drones in 5.645 GHz TRAIN ADENTRO
+    "background_5G_5645_hardneg_val", #150 images in real ambient environment without drones in 5.645 GHz VAL ADENTRO
+    "background_5G_5645_hardneg_test", #150 images in real ambient environment without drones in 5.645 GHz TEST AFUERA
+
 ]
 
 CLASS_MAP = {
     "autel": AUTEL_FOLDERS,
     "m30t": M30T_FOLDERS,
     "mavic4pro": MAVIC4_PRO_FOLDERS,
+    "mavic3pro": MAVIC3_PRO_FOLDERS,
     "fpv": FPV_FOLDERS,
     "background": BACKGROUND_FOLDERS,
 }
